cb856b4e3db338126ac17d77e1ca0a0a115e45ec
Omkar <omkarchebale0@gmail.com>
2024-03-29 21:07:38 +0530

    installed shadcn

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -51,7 +51,7 @@ const Auth = () => {
 
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat  bg-center bg-fixed bg-cover ">
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat  bg-center bg-fixed bg-cover  ">
       <div className=" sm:bg-opacity-50 w-full h-full lg:bg-opacity-50 md:bg-opacity-50">
         <nav className="px-12 py-5">
           <img src="/images/logo.png" alt="logo" className="h-12" />
