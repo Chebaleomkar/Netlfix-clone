554fb96445d68f054e40c2d153ea35ebfc730919
Omkar <omkarchebale0@gmail.com>
2024-07-13 17:49:25 +0530

    uninstall loadash and profile error fixed

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,3 +1,4 @@
+
 import useCurrentUser from "@/hooks/userCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
@@ -20,19 +21,19 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-const profiles = () => {
- const router = useRouter();
+const Profiles = () => {
+  const router = useRouter();
   const { data: user } = useCurrentUser();
 
   return (
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
         <h1 className="text-3xl md:text-6xl text-white text-center">
-          Who is waching?
+          Who is watching?
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
-          <div onClick={() =>router.push('/')  }>
-            <div className="group flex-row w-44 mx-auto  ">
+          <div onClick={() => router.push("/")}>
+            <div className="group flex-row w-44 mx-auto">
               <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
                 <img src="/images/default-blue.png" alt="profile" />
               </div>
@@ -48,4 +49,4 @@ const profiles = () => {
   );
 };
 
-export default profiles;
+export default Profiles;
