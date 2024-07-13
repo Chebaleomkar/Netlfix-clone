3c9b03e5099533a8ea54b9e663b6a709032a8d88
Omkar <omkarchebale0@gmail.com>
2024-07-13 17:54:59 +0530

    loadash unused

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,37 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import MovieCard from './MovieCard';
+import MovieCard from "./MovieCard";
 
-interface movieListProps{
-    data : Record<string , any>[];
-    title : string;
+interface movieListProps {
+  data: Record<string, any>[];
+  title: string;
 }
 
-const MovieList: React.FC<movieListProps> = ({data , title}) => {
-  
-  if(!data){
+const MovieList: React.FC<movieListProps> = ({ data, title }) => {
+  if (!data) {
     return null;
   }
 
   return (
-    <div className='px-4 md:px-12 mt-4 space-y-8' >
-        <div>
-            <p className='text-white text-md md:text-xl lg:text-2xl font-semibold mb-4 ' >
-                {title || 'Title'}
-            </p>
-            <div className='grid grid-cols-4 gap-2'>
-              {
-                data.map((movie) =>(
-                    <div key={movie.id} > 
-                        <MovieCard key={movie.id} data={movie}  />
-                    </div>
-                ))
-              }
+    <div className="px-4 md:px-12 mt-4 space-y-8">
+      <div>
+        <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4 ">
+          {title || "Title"}
+        </p>
+        <div className="grid grid-cols-4 gap-2">
+          {data.map((movie) => (
+            <div key={movie.id}>
+              <MovieCard key={movie.id} data={movie} />
             </div>
+          ))}
         </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieList
+export default MovieList;
